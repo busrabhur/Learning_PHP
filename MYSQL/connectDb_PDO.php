521b590971ba7d4cc09fc3cb0ecb87ce58108379
Büşra Buhur <61162439+busrabhur@users.noreply.github.com>
2021-08-26 16:24:41 +0300

    .

---
+++ MYSQL/connectDb_PDO.php
@@ -0,0 +1,34 @@
+<?php 
+
+try {
+	//$baglanti=new PDO("mysql:host:localhost;dbname:bağlanmakIstediğinDBismi",'mysql kullanıcı adı','mysql şifre')
+	/*Xampp ve diğer eşdeğer programların hepsinde admin ismi root dur.Xampp de şifre yoktur.Başka programlarda var onları kullanınca şifre de yazarız.Şimdilik boş bıraktık.*/
+
+	$db=new PDO("mysql:host=localhost; dbname=deneme",'root','');
+	echo"Veritabanı bağlantısı sağlandı.";
+	
+} catch (Exception $e) {
+	//bağlantı sağlanamadıysa catch e girer.Sebebini getmesagge söyler
+	echo $e->getmessage();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
