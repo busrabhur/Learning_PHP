f2cdbb6c2796bc68e0b3bfa178dbe669d430157b
Büşra Buhur <61162439+busrabhur@users.noreply.github.com>
2021-09-08 22:08:27 +0300

    Update cookieOlusturControlEt.php

--- COOKIE/cookieOlusturControlEt.php
+++ COOKIE/cookieOlusturControlEt.php
@@ -7,7 +7,7 @@
  setcookie("isim",$name,time()+60);//saniye değeri,oluşturulduğu andan kaç saniye sonra öleceğini bu method içinde belirleriz.
  setcookie("sifre",$password,time()+60);//şimdiki zamandan 60 saniye sonrasına kadar ömrü olur.(sayfa tarayıcıda ilk açıldığı andan sonra)
   
- echo"KAYITLI ÇEREZLER:<br><br>";
+ echo"KAYITLI ÇEREZLERİ GOSTER:<br><br>";
  echo "isim çerezindeki veri:".$_COOKIE['isim'];
  echo "<br> şifre çerezindeki veri:".$_COOKIE['sifre'];
  echo "<br><br><a href='cookieSil.php'>Cookileri Sil</a>";
